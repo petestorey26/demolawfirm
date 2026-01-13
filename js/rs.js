4bb0019e50be11175927a7eca32c6e1f84a5e275
ReviewSolicitors <developers@reviewsolicitors.com>
2026-01-13 13:21:38 +0000

    change source to lawyerup.co.uk

--- js/rs.js
+++ js/rs.js
@@ -1,6 +1,6 @@
 (function (rs) {
-    var endpointUrl = 'https://www2.reviewsolicitors.co.uk/';
-    //var endpointUrl = 'http://localhost:8080/';
+    var endpointUrl = 'https://www.lawyerup.co.uk/';
+    // var endpointUrl = 'http://localhost:8080/';
     // var endpointUrl = 'https://rsstaging.prevs.net/';
 
     var widgetUrl = endpointUrl + 'widget/';
